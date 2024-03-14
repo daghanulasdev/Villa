11122a5d9c95606666129ed1ac0ac98392c19e55
ulasd <ulasd@DAGHANU>
2024-03-14 13:20:21 +0300

    BannerController complete and BannerViews add and complete

--- Villa.WebUI/Controllers/BannerController.cs
+++ Villa.WebUI/Controllers/BannerController.cs
@@ -1,7 +1,9 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
 using Villa.Business.Abstract;
 using Villa.Dto.Dtos.BannerDtos;
+using Villa.Entity.Entities;
 
 namespace Villa.WebUI.Controllers
 {
@@ -21,6 +23,40 @@ namespace Villa.WebUI.Controllers
             var values = await _bannerService.TGetListAsync();
             var bannerList = _mapper.Map<List<ResultBannerDto>>(values);
             return View(bannerList);
+        }
+
+        public async Task<IActionResult> DeleteBanner(ObjectId id)
+        {
+            await _bannerService.TDeleteAsync(id);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult CreateBanner()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateBanner(CreateBannerDto createBannerDto)
+        {
+            var newBanner = _mapper.Map<Banner>(createBannerDto);
+            await _bannerService.TCreateAsync(newBanner);
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> UpdateBanner(ObjectId id)
+        {
+            var values = await _bannerService.TGetByIdAsync(id);
+            var updateBanner = _mapper.Map<UpdateBannerDto>(values);
+            return View(updateBanner);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateBanner(UpdateBannerDto updateBannerDto)
+        {
+            var banner = _mapper.Map<Banner>(updateBannerDto);
+            await _bannerService.TUpdateAsync(banner);
+            return RedirectToAction("Index");
         }
     }
 }
