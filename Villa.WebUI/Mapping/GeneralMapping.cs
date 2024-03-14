e9662c0240a66fcb27786b40d25d10933cad63ce
ulasd <ulasd@DAGHANU>
2024-03-14 15:40:38 +0300

    Deal,feature,product dtos and mapper add

--- Villa.WebUI/Mapping/GeneralMapping.cs
+++ Villa.WebUI/Mapping/GeneralMapping.cs
@@ -3,6 +3,9 @@ using System.Diagnostics.Metrics;
 using Villa.Dto.Dtos.BannerDtos;
 using Villa.Dto.Dtos.ContactDtos;
 using Villa.Dto.Dtos.CounterDtos;
+using Villa.Dto.Dtos.DealDtos;
+using Villa.Dto.Dtos.FeatureDtos;
+using Villa.Dto.Dtos.ProductDtos;
 using Villa.Entity.Entities;
 
 namespace Villa.WebUI.Mapping
@@ -23,7 +26,19 @@ namespace Villa.WebUI.Mapping
             CreateMap<ResultCounterDto, Counter>().ReverseMap();
             CreateMap<UpdateCounterDto, Counter>().ReverseMap();
 
-            
+            CreateMap<CreateDealDto, Deal>().ReverseMap();
+            CreateMap<ResultDealDto, Deal>().ReverseMap();
+            CreateMap<UpdateDealDto, Deal>().ReverseMap();
+
+            CreateMap<CreateFeatureDto, Feature>().ReverseMap();
+            CreateMap<ResultFeatureDto, Feature>().ReverseMap();
+            CreateMap<UpdateFeatureDto, Feature>().ReverseMap();
+
+            CreateMap<CreateProductDto, Product>().ReverseMap();
+            CreateMap<ResultProductDto, Product>().ReverseMap();
+            CreateMap<UpdateProductDto, Product>().ReverseMap();
+
+
         }
     }
 }
