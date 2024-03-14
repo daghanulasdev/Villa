98ab07d622fb6e5aca394535bcbc7bb2a20be59b
ulasd <ulasd@DAGHANU>
2024-03-14 14:39:04 +0300

    viewcomponents complete

---
+++ Villa.WebUI/ViewComponents/AdminLayout/_AdminFooter.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Villa.WebUI.ViewComponents.AdminLayout
+{
+    public class _AdminFooter : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        { return View(); }
+    }
+}
---
+++ Villa.WebUI/ViewComponents/AdminLayout/_AdminScript.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Villa.WebUI.ViewComponents.AdminLayout
+{
+    public class _AdminScript : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        { return View(); }
+    }
+}
