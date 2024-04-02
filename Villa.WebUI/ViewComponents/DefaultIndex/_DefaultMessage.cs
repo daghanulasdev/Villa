d4f9c6a540806ccf4695ab4bbf07f0c3efea8a0f
ulasd <ulasd@DAGHANU>
2024-04-02 15:49:40 +0300

    UI all sections done.

---
+++ Villa.WebUI/ViewComponents/DefaultIndex/_DefaultMessage.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Villa.WebUI.ViewComponents.DefaultIndex
+{
+    public class _DefaultMessage : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+            { return View(); }
+    }
+}
